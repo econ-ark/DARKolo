b4d2ef5cebe6f68ae3284441c6f7c3f7f4c6d4e0
llorracc <llorracc@users.noreply.github.com>
2019-11-07 17:40:20 -0500

    Split HARK and DolARK - per conv w Pablo

--- chimeras/BufferStock/BufferStock-HARK.py
+++ chimeras/BufferStock/BufferStock-HARK.py
@@ -15,12 +15,10 @@
 # ---
 
 # %% [markdown]
-# # Buffer Stock Saving in HARK and dolo
-# <!-- <p style="text-align: center;"><small><small>Generator: BufferStockTheory-make/notebooks_byname</small></small></p>
-# -->
+# # Buffer Stock Saving in HARK
 
 # %% [markdown]
-# This notebook compares the solutions to a standard buffer stock saving model obtained by the [Econ-ARK/HARK](https://github.com/econ-ark/HARK) toolkit and the [dolo](https://github.com/EconForge/dolo) modeling system.
+# This notebook constructs solutions to a standard buffer stock saving model obtained by the [Econ-ARK/HARK](https://github.com/econ-ark/HARK) toolkit
 
 # %% {"code_folding": [0]}
 # This cell does some setup and imports generic tools 
@@ -152,83 +150,13 @@ if Generator:
 # For the purposes of this notebook, [the paper's](http://econ.jhu.edu/people/ccarroll/papers/BufferStockTheory) baseline [parameterization](http://www.econ2.jhu.edu/people/ccarroll/papers/BufferStockTheory/#Baseline-Numerical-Solution) is changed as follows:
 #
 # 1. The unemployment (zero-income event) shocks are turned off
-# 2. An explicit liqudity constraint is added ($c_{t} \leq m_{t}$); that is, the consumer is prohibited from borrowing, and consumption must always be greater than zero
-
-# %% [markdown]
-# # Dolo
-#
-# [Installation instructions](https://github.com/EconForge/dolo/wiki/Installation) for dolo involve a number of dependencies, including the dolo language "dolang."  This notebook assumes all these have been installed.
-
-# %%
-import dolang
-from dolo import *
-
-# %% [markdown]
-# Dolo defines models using "model files" whose syntax is specified in the documentation
-
-# %%
-import sys
-import os
-os.getcwd()
-
-# %%
-model_dolo = yaml_import("./bufferstock.yaml")
-print( model_dolo )
-
-# Obtain the decision rule by time iteration
-dr = time_iteration(model_dolo,tol=1e-08,verbose=True)
-
-# %% [markdown]
-# ## Explaining the Dolo Model File
-#
-# Each item in the dolo yaml file corresponds to an equation in the formal mathematics of the BufferStockTheory problem.
-#
-# ### arbitrage
-#
-# The first such item we want to consider is labelled `arbitrage`.  In Dolo, the arbitrage equation represents a condition that should be equal to zero for an optimizing agent.  Furthermore, items with a future date (like `c(1)` which is equal to $c_{t+1}$ in the usual mathematical notation) are implicitly items whose expectation is being taken.  Dolo's specification of the model defines `perm` as the logarithm of what is called $\psi$, so `exp(perm(1))` is equivalent to $\psi_{t+1}$.  Finally, the expression `| 0.0 <= c <= m` corresponds to the liquidity constraint mentioned above (mathematically, $0 \leq c \leq m$). Thus, the equation below is how dolo represents the equation above:
-#
-#     arbitrage:
-#         - (R*β*((c(1)*exp(perm(1))*Γ)/c)^(-ρ)-1 ) | 0.0<=c<=m
-#
-# This equation is derived from the Euler equation described above, which can be rewritten as:
-#
-# \begin{align*}
-# c_{t}^{-\rho} & = R \beta \mathbb{E}_{t}[(\Gamma \psi c_{t+1})^{-\rho})] \\
-# % 0 & = & R \beta \mathbb{E}_{t}[(\Gamma \psi c_{t+1}/c_{t})^{-\rho})]-1
-# 1 & = R \beta \mathbb{E}_{t}[(\Gamma \psi \frac{c_{t+1}}{c_{t}})^{-\rho})] 
-# \\
-# 0 & = R \beta \mathbb{E}_{t}[(\Gamma \psi c_{t+1}/c_{t})^{-\rho})]-1
-# \end{align*}
-#
-# ### transition
-#
-# The second equation in the `dolang` model is:
-#
-#     transition:
-#         - m = exp(tran) + (m(-1)-c(-1))*(R/(Γ*exp(perm)))
-#
-# Recall that the Bellman form of the original problem is:
-#
-# \begin{eqnarray*}
-# v_t(m_t) &=& \max_{c_t}~~ u(c_t) + \beta~\mathbb{E}_{t} [(\Gamma\psi_{t+1})^{1-\rho} v_{t+1}(m_{t+1}) ] \\
-# & s.t. & \\
-# a_t &=& m_t - c_t \\
-# m_{t+1} &=& R/(\Gamma \psi_{t+1}) a_t + \theta_{t+1} \\
-# \end{eqnarray*}
-#
-# Substituting the first condition, defining $a_t$, into the second, and rearranging, we get:
-#
-# \begin{eqnarray}
-# m_{t+1} &=& \theta_{t+1} +  (m_t - c_t) R/(\Gamma \psi_{t+1})
-# \end{eqnarray}
-#
-# In the `dolang` model, `tran == 1.0`; this is the mean of the transitory shock $\theta$. `exp(tran)` is $e$, the Euler number.
+# 2. An explicit liqudity constraint is added ($c_{t} \leq m_{t}$); that is, the consumer is prohibited from borrowing
 
 # %% [markdown]
 # # HARK
 # The [Econ-ARK/HARK](https://github.com/econ-ark/HARK) toolkit's solution to this problem is part of the $\texttt{ConsIndShockModel.py}$ module in the $\texttt{ConsumptionSaving}$ directory of tools.  For an introduction to this module, see the [ConsIndShockModel.ipynb](https://econ-ark.org/notebooks) notebook at the [Econ-ARK](https://econ-ark.org) website.
 
-# %% {"code_folding": []}
+# %% {"code_folding": [0]}
 # Define a parameter dictionary with baseline parameter values
 
 # Set the baseline parameter values 
@@ -265,7 +193,7 @@ base_params['BoroCnstArt']  = None    # No artificial borrowing constraint
 
 from HARK.utilities import plotFuncsDer, plotFuncs
 from HARK.ConsumptionSaving.ConsIndShockModel import IndShockConsumerType
-# %% {"code_folding": []}
+# %% {"code_folding": [0]}
 # Create a model identical to the dolo model
 # Start with the HARK baseline parameters and modify 
 # to be like the dolo model 
@@ -274,20 +202,20 @@ base_params_dolo['BoroCnstArt']  = 0.0    # Liquidity constraint at 0
 base_params_dolo['UnempPrb']     = 0      # No point-mass on unemployment state 
 base_params_dolo['TranShkCount'] = 5      # Default number of nodes in dolo
 base_params_dolo['PermShkCount'] = 5
-base_params_dolo['aXtraMax']     = 500  # Use same maximum
+base_params_dolo['aXtraMax']     = max_m  # Use same maximum
 base_params_dolo['aXtraCount']   = 100    # How dense to make the grid
 base_params_dolo['DiscFac']      = 0.96
 #base_params_dolo['CubicBool']    = False
 model_HARK = IndShockConsumerType(**base_params_dolo,cycles=0) # cycles=0 indicates infinite horizon
 
-# %% {"code_folding": []}
+# %% {"code_folding": [0]}
 # Solve the HARK model 
 model_HARK.updateIncomeProcess()
 model_HARK.solve()
 model_HARK.UnempPrb = 0.05
 model_HARK.unpackcFunc()
 
-# %% {"code_folding": []}
+# %% {"code_folding": [0]}
 # Plot the results: Green is perfect foresight, red is HARK, black is dolo
 
 tab = tabulate(model_dolo, dr, 'm')
@@ -298,68 +226,3 @@ c_m  = model_HARK.cFunc[0](m)
 cPF = (np.array(m)-1+1/(1-PermGroFac/Rfree))*((Rfree-(Rfree * DiscFac)**(1/CRRA))/Rfree)
 plt.plot(tab['m'],c_m,color="red")
 plt.plot(m,cPF,color="green")
-
-# %% [markdown]
-# ### [Upper and Lower Limits of the Marginal Propensity to Consume](https://econ.jhu.edu/people/ccarroll/papers/BufferStockTheory/#MPCLimits)
-#
-# The paper shows that as $m_{t}~\uparrow~\infty$ the consumption function in the presence of risk gets arbitrarily close to the perfect foresight consumption function.  Defining \underline{κ}
-# as the perfect foresight model's MPC, this implies that $\lim_{m_{t}~\uparrow~\infty} c^{\prime}(m) = $ \underline{κ}
-# .
-#
-# The paper also derives an analytical limit $\bar{\kappa}$ for the MPC as $m$ approaches 0., its bounding value.  Strict concavity of the consumption function implies that the consumption function will be everywhere below a function $\bar{\kappa}m$, and strictly declining everywhere.  The last figure plots the MPC between these two limits.
-
-# %%
-# Define k_lower, h_inf and perfect foresight consumption function, upper bound of consumption function and lower
-# bound of consumption function.
-k_lower = 1.0-(model_HARK.Rfree**(-1.0))*(model_HARK.Rfree*model_HARK.DiscFac)**(1.0/model_HARK.CRRA)
-
-
-# %%
-# The last figure shows the upper and lower limits of the MPC
-plt.figure(figsize = (12,8))
-# Set the plot range of m
-m = np.linspace(0.001,8,1000)
-
-# Use the HARK method derivative to get the derivative of cFunc, and the values are just the MPC
-MPC = model_HARK.cFunc[0].derivative(m)
-
-# Define the upper bound of MPC
-MPCUpper = (1 - model_HARK.UnempPrb ** (1.0/model_HARK.CRRA)*(model_HARK.Rfree*model_HARK.DiscFac)**(1.0/model_HARK.CRRA)/model_HARK.Rfree)
-
-# Define the lower bound of MPC
-MPCLower = k_lower
-
-kappaDef=r'$\underline{\kappa}$'
-if not iflatexExists:
-    kappaDef=r'κ̲$\equiv(1-\Phi_{R})$'
-
-plt.plot(m,MPC,color = 'black')
-plt.plot([0,8],[MPCUpper,MPCUpper],color = 'black')
-plt.plot([0,8],[MPCLower,MPCLower],color = 'black')
-plt.xlim(0,8)
-plt.ylim(0,1)
-plt.text(1.5,0.6,r'$\kappa(m) \equiv c^{\prime}(m)$',fontsize = 26,fontweight='bold')
-plt.arrow(1.45,0.61,-0.4,0,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-
-if iflatexExists:
-    plt.text(5,.82,r'$\overline{\kappa}$',fontsize = 26,fontweight='bold')
-else:
-    plt.text(5,0.82,r'$(1-\wp^{1/\rho}\Phi_{R})\equiv \overline{\kappa}$',fontsize = 26,fontweight='bold')
-plt.arrow(4.95,0.825,-0.2,-0.03,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-
-plt.text(0.5,0.07,kappaDef,fontsize = 26,fontweight='bold')
-plt.text(8.05,0,"$m$",fontsize = 26)
-plt.arrow(.8,0.07,0.2,-0.01,head_width= 0.02,width=0.001,facecolor='black',length_includes_head='True')
-
-if Generator:
-    plt.savefig(os.path.join(Figures_HARK_dir, 'MPCLimits.png'))
-    plt.savefig(os.path.join(Figures_HARK_dir, 'MPCLimits.jpg'))
-    plt.savefig(os.path.join(Figures_HARK_dir, 'MPCLimits.pdf'))
-    plt.savefig(os.path.join(Figures_HARK_dir, 'MPCLimits.svg'))
-if not in_ipynb():
-    plt.show(block=False) 
-    plt.pause(1)
-else:
-    plt.show(block=True) # Change to False if you want to run uninterrupted
-
-# %%
